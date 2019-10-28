5e3d69402c099a79c9b506f9588844884e729bc3
s16673 <56188105+s16673@users.noreply.github.com>
2019-10-28 20:33:33 +0100

    25_10_2019
    Pliki związane z EPG (z zajęć z Unity + pliki ze sprite'ami do gry) stworzone do 25.10.2019r.

---
+++ Zajecia/Unity/New Unity Project/Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 500;
+    public float jumpSpeed = 400;
+
+    private Rigidbody2D rb;
+    private float xDisplacement;
+    private bool isGrounded;
+
+    // public to zmiana dostępna z poziomu Unity, udzielając możliwości zmiany wartości w interfejsie Unity i to ona będzie decydująca;
+    // private nie jest dostępne z Unity, możemy to zapisać w metodzie start
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = false;
+    }
+
+    void Update()
+    {
+        xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 vector = new Vector2(xDisplacement, rb.velocity.y);
+        rb.velocity = vector;
+
+        if(Input.GetKeyDown("space"))
+        {
+            if(isGrounded == true)
+            {
+                rb.AddForce(new Vector2(0, jumpSpeed));
+                isGrounded = false;
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
